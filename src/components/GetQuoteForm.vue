fac5fa2882357873dbfc3b1907dbf5ccfc80b0b2
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-05-25 19:49:23 +0200

    form validation + style updates

--- src/components/GetQuoteForm.vue
+++ src/components/GetQuoteForm.vue
@@ -1,62 +1,145 @@
 <script setup>
 import ButtonEl from '../template/ButtonEl.vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength, maxLength, helpers } from '@vuelidate/validators'
+import { reactive } from 'vue'
+
+const state = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  country: '',
+  modelSupply: '',
+  service: [],
+  message: ''
+})
+
+const rules = {
+  firstName: { required, minLength: minLength(3), maxLength: maxLength(15), $autoDirty: true },
+  lastName: { required, minLength: minLength(3), maxLength: maxLength(15), $autoDirty: true },
+  email: { required, email, maxLength: maxLength(40), $autoDirty: true },
+  country: { required, minLength: minLength(3), maxLength: maxLength(15), $autoDirty: true },
+  modelSupply: { required: helpers.withMessage('Select an option.', required) },
+  service: { required: helpers.withMessage('At least one option must be selected.', required) },
+  message: { required, minLength: minLength(3), maxLength: maxLength(1000), $autoDirty: true }
+}
+
+const v = useVuelidate(rules, state)
+
+const handleForm = async () => {
+  const isFormCorrect = await v.value.$validate()
+  console.log(state)
+
+  if (!isFormCorrect) return
+}
 </script>
 
 <template>
   <form class="form" @submit.prevent="handleForm">
     <section class="form__section">
-      <input class="form__input" type="text" id="name" placeholder="Enter name..." />
-      <label class="form__label" for="name">Name:</label>
+      <input
+        class="form__input"
+        type="text"
+        id="name"
+        placeholder="Enter name..."
+        @blur="v.firstName.$touch"
+        v-model.lazy="state.firstName"
+      />
+      <label class="form__label" for="name">First Name:</label>
+      <p class="error" v-if="v.firstName.$error">{{ v.firstName.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <input class="form__input" type="text" id="last-name" placeholder="Enter last name..." />
+      <input
+        class="form__input"
+        type="text"
+        id="last-name"
+        placeholder="Enter last name..."
+        @blur="v.lastName.$touch"
+        v-model.lazy="state.lastName"
+      />
       <label class="form__label" for="last-name">Last Name:</label>
+      <p class="error" v-if="v.lastName.$error">{{ v.lastName.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <input class="form__input" type="email" id="email" placeholder="Enter email..." />
+      <input
+        class="form__input"
+        type="email"
+        id="email"
+        placeholder="Enter email..."
+        @blur="v.email.$touch"
+        v-model.lazy="state.email"
+      />
       <label class="form__label" for="email">Email address:</label>
+      <p class="error" v-if="v.email.$error">{{ v.email.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <input class="form__input" type="text" id="country" placeholder="Enter country..." />
+      <input
+        class="form__input"
+        type="text"
+        id="country"
+        placeholder="Enter country..."
+        @blur="v.country.$touch"
+        v-model.lazy="state.country"
+      />
       <label class="form__label" for="country">Country:</label>
+      <p class="error" v-if="v.country.$error">{{ v.country.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section form__section-select">
       <div class="select-wrapper">
-        <select id="supply" class="form__select">
-          <option value="Shop" selected>Shop Supply</option>
+        <select id="supply" class="form__select" v-model="state.modelSupply">
+          <option disabled selected value>-- select an option --</option>
+          <option value="Other">Other - describe in message</option>
+          <option value="Shop">Shop Supply</option>
           <option value="Customer">Client Supply</option>
         </select>
         <label class="form__select-label" for="supply">How are you supplying us with models?</label>
+        <p class="error" v-if="v.modelSupply.$error">{{ v.modelSupply.$errors[0].$message }}</p>
       </div>
     </section>
 
     <section class="form__checkboxes">
       <p class="form__checkboxes-heading">We offer:</p>
       <div class="form__checkbox">
-        <input class="checkbox" type="checkbox" name="assemble" id="assemble" />
+        <input
+          class="checkbox"
+          type="checkbox"
+          value="assemble"
+          id="assemble"
+          v-model="state.service"
+        />
         <label class="form__label" for="assemble">Assemble</label>
       </div>
       <div class="form__checkbox">
-        <input type="checkbox" name="basing" id="basing" />
+        <input type="checkbox" value="basing" id="basing" v-model="state.service" />
         <label class="form__label" for="basing">Basing</label>
       </div>
       <div class="form__checkbox">
-        <input type="checkbox" name="painting" id="painting" />
+        <input type="checkbox" value="painting" id="painting" v-model="state.service" />
         <label class="form__label" for="painting">Painting</label>
       </div>
       <div class="form__checkbox">
-        <input type="checkbox" name="cleaning" id="cleaning" />
+        <input type="checkbox" value="cleaning" id="cleaning" v-model="state.service" />
         <label class="form__label" for="cleaning">Cleaning</label>
       </div>
+      <p class="error" v-if="v.service.$error">{{ v.service.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <textarea class="form__input" type="text" id="message" placeholder="Message" rows="10" />
+      <textarea
+        class="form__input resize"
+        type="text"
+        id="message"
+        placeholder="Message"
+        rows="10"
+        @blur="v.message.$touch"
+        v-model.lazy="state.message"
+      />
       <label class="form__label" for="message">Message:</label>
+      <p class="error" v-if="v.message.$error">{{ v.message.$errors[0].$message }}</p>
     </section>
     <ButtonEl class="form__btn btn--medium btn--outline-black btn--slide-black">Submit</ButtonEl>
   </form>
@@ -163,6 +246,13 @@ import ButtonEl from '../template/ButtonEl.vue'
         color: $color-black;
       }
     }
+  }
+
+  .resize {
+    resize: vertical;
+  }
+  .error {
+    @include error;
   }
 }
 </style>
--- src/components/QuestionForm.vue
+++ src/components/QuestionForm.vue
@@ -1,33 +1,85 @@
 <script setup>
 import ButtonEl from '../template/ButtonEl.vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength, maxLength } from '@vuelidate/validators'
+import { reactive } from 'vue'
+
+const state = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  message: ''
+})
+
+const rules = {
+  firstName: { required, minLength: minLength(3), maxLength: maxLength(15), $autoDirty: true },
+  lastName: { required, minLength: minLength(3), maxLength: maxLength(15), $autoDirty: true },
+  email: { required, email, maxLength: maxLength(40), $autoDirty: true },
+  message: { required, minLength: minLength(3), maxLength: maxLength(1000), $autoDirty: true }
+}
+
+const v = useVuelidate(rules, state)
+
+const handleForm = async () => {
+  const isFormCorrect = await v.value.$validate()
+
+  if (!isFormCorrect) return
+}
 </script>
 
 <template>
   <form class="form" @submit.prevent="handleForm">
     <section class="form__section">
-      <input class="form__input" type="text" id="name" placeholder="Enter name..." />
-      <label class="form__label" for="name">Name:</label>
+      <input
+        class="form__input"
+        type="text"
+        id="name"
+        placeholder="Enter name..."
+        v-model.lazy="state.firstName"
+        @blur="v.firstName.$touch"
+      />
+      <label class="form__label" for="name">First Name:</label>
+      <p class="error" v-if="v.firstName.$error">{{ v.firstName.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <input class="form__input" type="text" id="last-name" placeholder="Enter last name..." />
+      <input
+        class="form__input"
+        type="text"
+        id="last-name"
+        placeholder="Enter last name..."
+        v-model.lazy="state.lastName"
+        @blur="v.lastName.$touch"
+      />
       <label class="form__label" for="last-name">Last Name:</label>
+      <p class="error" v-if="v.lastName.$error">{{ v.lastName.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
-      <input class="form__input" type="email" id="email" placeholder="Enter email..." />
+      <input
+        class="form__input"
+        type="text"
+        id="email"
+        placeholder="Enter email..."
+        v-model.lazy="state.email"
+        @blur="v.email.$touch"
+      />
       <label class="form__label" for="email">Email address:</label>
+      <p class="error" v-if="v.email.$error">{{ v.email.$errors[0].$message }}</p>
     </section>
 
     <section class="form__section">
       <textarea
-        class="form__input"
+        class="form__input resize"
         type="text"
         id="message"
         placeholder="What is your question?"
         rows="10"
+        v-model.lazy="state.message"
+        @blur="v.message.$touch"
       />
       <label class="form__label" for="message">Message:</label>
+      <p class="error" v-if="v.message.$error">{{ v.message.$errors[0].$message }}</p>
     </section>
     <ButtonEl class="form__btn btn--medium btn--outline-black btn--slide-black">Submit</ButtonEl>
   </form>
@@ -48,6 +100,10 @@ import ButtonEl from '../template/ButtonEl.vue'
   .form__btn {
     justify-self: auto;
     align-self: auto;
+  }
+
+  .resize {
+    resize: vertical;
   }
 }
 </style>
